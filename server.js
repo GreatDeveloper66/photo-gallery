1861173bdf9ed74bc8917c670b03e3ff18e08cc8
GreatDeveloper66 <ashaffe@gmail.com>
2022-12-29 13:26:37 -0600

    Add port listening to server.js

--- server.js
+++ server.js
@@ -6,3 +6,6 @@ const app = express()
 const port = process.env.PORT || 5000
 
 app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.listen(port, () => console.log(`Listening on port ${port}`))
