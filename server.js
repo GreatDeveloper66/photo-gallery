7287e306da2d29e284a859ceecc613ae9edd06d4
GreatDeveloper66 <ashaffe@gmail.com>
2023-01-01 18:23:20 -0600

    add express static line to server.js

--- server.js
+++ server.js
@@ -7,13 +7,15 @@ const port = process.env.PORT || 5000
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static('public'))
 
 app.get('/api/hello', (req,res) => {
     res.send({ express: 'Hello from express on the hello route'})
 })
 
-app.get('/', (req,res) => {
-    res.send({ express: 'Hello welcome to the main route'})
+app.get('/api/world', (req,res) => {
+    console.log(req.body)
+    res.send(`I received your POST request. This is what you sent me: ${req.body.post}`)
 })
 
 app.listen(port, () => console.log(`Listening on port ${port}`))
